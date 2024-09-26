2117b680c17ad515e68ebf47fc5ba94716da0475
CaiocrNyimi <caiocrnyimi@gmail.com>
2024-09-25 23:03:50 -0300

    teste json

--- aula14/src/app/produtos/page.tsx
+++ aula14/src/app/produtos/page.tsx
@@ -68,39 +68,39 @@ export default function Produtos() {
       <h1>Produtos</h1>
       <table>
         <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Preço</th>
-                <th>Marca</th>
-                <th>Cor</th>
-                <th>Modelo</th>
-                <th>Quantidade</th>
-                <th>Categoria</th>
-                <th>Imagem</th>
-                <th>Editar</th>
-            </tr>
+          <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Marca</th>
+            <th>Cor</th>
+            <th>Modelo</th>
+            <th>Quantidade</th>
+            <th>Categoria</th>
+            <th>Imagem</th>
+            <th>Editar</th>
+          </tr>
         </thead>
 
         <tbody>
-            {produtos.map((produto) => (
-                <tr key={produto.id}>
-                    <td>{produto.nome}</td>
-                    <td>{produto.preco}</td>
-                    <td>{produto.marca}</td>
-                    <td>{produto.cor}</td>
-                    <td>{produto.modelo}</td>
-                    <td>{produto.quantidade}</td>
-                    <td>{produto.categoria}</td>
-                    <td><img src={produto.imagem} alt="Imagem do produto" width="100" height="100" /></td>
-                    <td><Link href={`/produto/${produto.id}`}>Editar</Link></td>
-                </tr>
-                ))}
+          {produtos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.marca}</td>
+              <td>{produto.cor}</td>
+              <td>{produto.modelo}</td>
+              <td>{produto.quantidade}</td>
+              <td>{produto.categoria}</td>
+              <td><img src={produto.imagem} alt="Imagem do produto" width="100" height="100" /></td>
+              <td><Link href={`/produtos/produto/${produto.id}`}>Editar</Link></td>
+            </tr>
+          ))}
         </tbody>
-            <tfoot>
-                <tr>
-                    <td colSpan={8}>Total de Produtos: {produtos.length}</td>
-                </tr>
-            </tfoot>
+        <tfoot>
+          <tr>
+            <td colSpan={8}>Total de Produtos: {produtos.length}</td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   );
